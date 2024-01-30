6e6492f3bee96ee3d03c9f836e8124a7f0ea0e35
frunza <marius_carp@trimblee.com>
2024-01-30 10:07:12 +0200

    start with user service and hasing passwords

---
+++ modules/server/src/main/scala/com/frunza/reviewboard/services/UserService.scala
@@ -0,0 +1,97 @@
+package com.frunza.reviewboard.services
+
+import com.frunza.reviewboard.domain.data.User
+import com.frunza.reviewboard.repositories.UserRepository
+import com.frunza.reviewboard.services.UserServiceLive.Hasher
+import zio.*
+
+import java.security.SecureRandom
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+trait UserService {
+
+  def registerUser(email: String, password: String): Task[User]
+  def verifyPassword(email: String, password: String): Task[Boolean]
+
+}
+
+class UserServiceLive private(userRepo: UserRepository) extends UserService {
+
+  override def registerUser(email: String, password: String): Task[User] =
+    userRepo.create(
+      User (
+        id = -1L,
+        email = email,
+        hashedPassword = Hasher.generateHash(password)
+      )
+    )
+  override def verifyPassword(email: String, password: String): Task[Boolean] =
+    for {
+      existingUser <- userRepo
+        .getByEmail(email)
+        .someOrFail(new RuntimeException(s"cannot verify user $email: nonexistant"))
+      result <- ZIO.attempt(
+        Hasher.validateHash(password, existingUser.hashedPassword)
+      )
+    } yield result
+
+
+}
+
+object UserServiceLive {
+  val layer = ZLayer {
+    ZIO.service[UserRepository].map(repo => new UserServiceLive(repo))
+  }
+
+  object Hasher {
+    private val PBKDF2_ALGO: String = "PBKDF2WithHmacSHA512"
+    private val PBKDF2_ITERATIONS: Int = 1000
+    private val SALT_BYTE_SIZE: Int = 24
+    private val HASH_BYTE_SIZE: Int = 24
+    private val skf: SecretKeyFactory = SecretKeyFactory.getInstance(PBKDF2_ALGO)
+
+    def generateHash(string: String): String = {
+      val rng: SecureRandom = new SecureRandom()
+      val salt: Array[Byte] = Array.ofDim[Byte](SALT_BYTE_SIZE)
+      rng.nextBytes(salt) // create 24 ransom bytes
+      val hashBytes = pbkdf2(string.toCharArray, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE)
+
+      s"$PBKDF2_ITERATIONS:${toHex(salt)}:${toHex(hashBytes)}"
+    }
+
+    def validateHash(string: String, hash: String): Boolean = {
+      val hashSegments = hash.split(":")
+      val nIterations = hashSegments(0).toInt
+      val salt = fromHex(hashSegments(1))
+      val validHash = fromHex(hashSegments(2))
+
+      val testHash = pbkdf2(string.toCharArray, salt, nIterations, HASH_BYTE_SIZE)
+
+      compareBytes(testHash, validHash)
+    }
+    
+    private def compareBytes(a: Array[Byte], b: Array[Byte]): Boolean = {
+      val range = 0 until math.min(a.length, b.length)
+      val diff = range.foldLeft(a.length ^ b.length) {
+        case (acc, i) => acc | (a(i) ^ b(i))
+      }
+      
+      diff == 0
+    }
+
+    private def pbkdf2(message: Array[Char], salt: Array[Byte], iterations: Int, nBytes: Int): Array[Byte] = {
+      val keySpec: PBEKeySpec = new PBEKeySpec(message, salt, iterations, nBytes * 8)
+      skf.generateSecret(keySpec).getEncoded
+    }
+
+    private def fromHex(string: String): Array[Byte] = {
+      string.sliding(2, 2).toArray.map { hexValue =>
+        Integer.parseInt(hexValue, 16).toByte
+      }
+    }
+
+    private def toHex(array: Array[Byte]): String =
+      array.map(b => "%02X".format(b)).mkString
+  }
+}
